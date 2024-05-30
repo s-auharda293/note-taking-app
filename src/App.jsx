9a26101c7512ccde0e933cca2e141543bf7d67f4
s-auharda293 <077bct036.suharda@sagarmatha.edu.np>
2024-05-30 20:17:38 +0545

    Notes added dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,122 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const todayDate = new Date();
+const day = todayDate.getDate();
+const month = todayDate.getMonth() + 1;
+const year = todayDate.getFullYear();
+
+let Notes = [];
+
 const App = () => {
+  const [showColorButtons, setShowColorButtons] = useState(false);
+  const [notes, setNotes] = useState(Notes);
+
+  const handleShowColorButtons = () => {
+    setShowColorButtons((colorButtons) => !colorButtons);
+  };
+
+  const handleHoldColor = (e) => {
+    const selectedColor = e.target.className.split(" ")[1];
+    console.log(selectedColor);
+
+    const newNote = {
+      id: crypto.randomUUID(),
+      content: "This is a note.",
+      date: `${month}/${day}/${year}`,
+      color: `${selectedColor}`,
+    };
+
+    Notes = [...Notes, newNote];
+    setNotes((notes) => [...notes, newNote]);
+
+    // <Note />;
+  };
+
   return (
     <>
       <div className="center-container">
-        <SideBar />
-        <Main />
+        <SideBar
+          showColorButtons={showColorButtons}
+          onClickShowColorButtons={handleShowColorButtons}
+          onHoldColor={handleHoldColor}
+        />
+        <Main notes={notes} />
       </div>
     </>
   );
 };
 
-const SideBar = () => {
+const SideBar = ({
+  showColorButtons,
+  onClickShowColorButtons,
+  onHoldColor,
+}) => {
   return (
     <div className="sidebar">
       <h3>Memento</h3>
       <div className="button-container">
         <div className="top">
-          <button type="button" className="btn btn-primary">
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={onClickShowColorButtons}
+          >
             +
           </button>
         </div>
-        <div className="bottom">
-          <button type="button" className="color-btn btn-yellow"></button>
-          <button type="button" className="color-btn btn-orange"></button>
-          <button type="button" className="color-btn btn-purple"></button>
-          <button type="button" className="color-btn btn-blue"></button>
-          <button type="button" className="color-btn btn-green"></button>
-        </div>
+        {showColorButtons ? (
+          <div className="bottom">
+            <button
+              type="button"
+              className="color-btn btn-yellow"
+              onClick={(e) => {
+                onHoldColor(e);
+              }}
+            ></button>
+            <button
+              type="button"
+              className="color-btn btn-orange"
+              onClick={(e) => {
+                onHoldColor(e);
+              }}
+            ></button>
+            <button
+              type="button"
+              className="color-btn btn-purple"
+              onClick={(e) => {
+                onHoldColor(e);
+              }}
+            ></button>
+            <button
+              type="button"
+              className="color-btn btn-blue"
+              onClick={(e) => {
+                onHoldColor(e);
+              }}
+            ></button>
+            <button
+              type="button"
+              className="color-btn btn-green"
+              onClick={(e) => {
+                onHoldColor(e);
+              }}
+            ></button>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
 };
 
-const Main = () => {
+const Main = ({ notes }) => {
   return (
     <div className="main">
       <SearchBar />
       <h1 className="notes-heading">Notes</h1>
-      <NoteContainer />
+      <NoteContainer notes={notes} />
     </div>
   );
 };
@@ -60,24 +140,27 @@ const SearchBar = () => {
   );
 };
 
-const NoteContainer = () => {
+const NoteContainer = ({ notes }) => {
   return (
     <div className="notes-container">
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+      {notes.map((note) => (
+        <Note
+          content={note.content}
+          date={note.date}
+          color={note.color}
+          key={note.id}
+        />
+      ))}
     </div>
   );
 };
 
-const Note = () => {
+const Note = ({ content, date, color }) => {
   return (
-    <div className="note">
-      <span className="note-content">jlaskd;jf;lkajdsf</span>
+    <div className={`note ${color}`}>
+      <span className="note-content">{content}</span>
       <div className="note-info">
-        <span className="note-date">May 21,2020</span>
+        <span className="note-date">{date}</span>
         <div className="note-buttons">
           <button
             type="button"
@@ -126,6 +209,7 @@ const Note = () => {
               </div>
             </div>
             {/* <Modal /> */}
+
             <ion-icon name="create-outline"></ion-icon>
           </button>
           <button type="button" className="btn delete-button">
@@ -136,44 +220,5 @@ const Note = () => {
     </div>
   );
 };
-
-// const Modal = () => {
-//   <div
-//     className="modal fade"
-//     id="exampleModal"
-//     tabIndex="-1"
-//     aria-labelledby="exampleModalLabel"
-//     aria-hidden="true"
-//   >
-//     <div className="modal-dialog">
-//       <div className="modal-content">
-//         <div className="modal-header">
-//           <h1 className="modal-title fs-5" id="exampleModalLabel">
-//             Modal title
-//           </h1>
-//           <button
-//             type="button"
-//             className="btn-close"
-//             data-bs-dismiss="modal"
-//             aria-label="Close"
-//           ></button>
-//         </div>
-//         <div className="modal-body">...</div>
-//         <div className="modal-footer">
-//           <button
-//             type="button"
-//             className="btn btn-secondary"
-//             data-bs-dismiss="modal"
-//           >
-//             Close
-//           </button>
-//           <button type="button" className="btn btn-primary">
-//             Save changes
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>;
-// };
 
 export default App;
